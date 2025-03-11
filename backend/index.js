53cdb38eb4a696c835d411c1aa66446d659cc985
Piyushrathoree <meonpath008@gmail.com>
2025-03-11 23:59:18 +0530

    add backend server setup with Express and authentication routes

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import router from "./routes/auth.route.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+// app.use(cookieParser())
+app.use('/api/auth',router )
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`our server is running on ${PORT}`);
+    });
+});
