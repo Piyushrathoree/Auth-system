5846c75b62ad7b1ced673cf52a18d8f08cc98ed9
Piyushrathoree <meonpath008@gmail.com>
2025-03-12 23:32:28 +0530

    add password recovery routes for forgot and reset password functionality

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,14 +4,27 @@ import {
     loginUser,
     logout,
     verifyEmail,
+    forgotPassword,
+    resetPassword,
 } from "../controllers/auth.controller.js";
+import verifyToken from "../middleware/verifyToken.middleware.js";
 
 const router = Router();
 
+router.get("/check-auth", verifyToken, (req, res) => {
+    const user = req.user;
+    res.status(200).json({
+        success: true,
+        user,
+        message: "your are authenticated properly",
+    });
+});
 // ✅ Normal Authentication Routes
 router.post("/signup", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logout);
 
-router.post('/verify-email', verifyEmail)
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 export default router;
