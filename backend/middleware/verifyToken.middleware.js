ef62f5873f7c0c1c9d6ef21bb23752ff8b6a2500
Piyushrathoree <meonpath008@gmail.com>
2025-03-12 23:32:20 +0530

    add middleware to verify JWT tokens from cookies

---
+++ backend/middleware/verifyToken.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export default function verifyToken(req, res, next) {
+    const token = req.cookies;
+    if (!token) {
+        return res
+            .status(401)
+            .json({ success: false, message: "Access Denied" });
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ success: false, message: "Invalid Token" });
+    }
+}
