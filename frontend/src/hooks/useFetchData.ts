767dd6bb8f45ec851f472209b0f4ab572e63ecac
Piyushrathoree <meonpath008@gmail.com>
2025-04-07 01:39:06 +0530

    feat: Implement useFetchData hook for signup, login, and logout functionality

---
+++ frontend/src/hooks/useFetchData.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+export default function useFetchData() {
+    interface Props {
+        name: string;
+        email: string;
+        password: string;
+    }
+    type LoginProps = Omit<Props, "name">;
+
+    const URL = "http://localhost:3000/api/auth";
+
+    async function signup(props: Props) {
+        const { name, email, password } = props;
+        if (!name || !email || !password) {
+            throw new Error("entities not found ");
+        }
+        try {
+            const response = await axios.post(`${URL}/signup`, {
+                name,
+                email,
+                password,
+            });
+            const data = response.data;
+            return data;
+        } catch (error) {
+            throw new Error(`something went wrong: ${error}`);
+        }
+    }
+
+    async function login(props: LoginProps) {
+        const { email, password } = props;
+        if (!email || !password) {
+            throw new Error("entities not found ");
+        }
+        try {
+            const response = await axios.post(`${URL}/login`, {
+                email,
+                password,
+            });
+            const data = response.data;
+            return data;
+        } catch (error) {
+            throw new Error(`something went wrong: ${error}`);
+        }
+    }
+
+    async function logoutUser() {
+        try {
+            await axios.post(`${URL}/logout`);
+        } catch (error) {
+            console.error(error);
+            throw new Error("Failed to logout user");
+        }
+    }
+
+    return {
+        signup,
+        login,
+        logoutUser
+    };
+}
