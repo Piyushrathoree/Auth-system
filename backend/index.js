550fd136f8437069d6657e1170695aa9818a6c7d
Piyushrathoree <meonpath008@gmail.com>
2025-04-07 02:07:08 +0530

    Refactor code structure for improved readability and maintainability

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,17 @@ import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import router from "./routes/auth.route.js";
-
+import cors from 'cors'
 dotenv.config();
 const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Replace with frontend URL
+        credentials: true, // Allow cookies & auth headers
+    })
+);
+
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser())
